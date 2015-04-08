55cb7274d268f87fbfa382000b0fd32740291ec1
Andrew <abutler@psl.nmsu.edu>
2015-04-08 16:20:15 -0600

    #3: A little work on the debugger

--- src/main/java/org/observe/ObservableDebug.java
+++ src/main/java/org/observe/ObservableDebug.java
@@ -1,11 +1,42 @@
 package org.observe;
 
+import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 /** A utility class for debugging observables */
 public final class ObservableDebug {
+	public static class ObservableDerivation {
+		public final List<Object> bases;
+		public final Object derived;
+		public final String derivation;
+		public final Object function;
+
+		public ObservableDerivation(List<Object> b, Object d, String derive, Object f) {
+			super();
+			bases = b;
+			derived = d;
+			derivation = derive;
+			function = f;
+		}
+	}
+
+	public static class ObservableTag {
+		public final Object tagged;
+		public final String tag;
+		public final Object value;
+
+		public ObservableTag(Object tagged, String tag, Object value) {
+			super();
+			this.tagged = tagged;
+			this.tag = tag;
+			this.value = value;
+		}
+	}
+
 	/** Whether debugging is turned on. If this is false, this class will do nothing */
-	public static final boolean DEBUG_ON = true;
+	public static final boolean DEBUG_ON = "true".equalsIgnoreCase(System.getProperty("org.observe.debug"));
 
 	/**
 	 * Whether debugging is always on. If false and {@link #DEBUG_ON} is true, then debugging must be {@link #startDebug() activated} and
@@ -121,6 +152,13 @@ public final class ObservableDebug {
 	}
 
 	private static final ConcurrentHashMap<Thread, DebugState> theThreadDebug = new ConcurrentHashMap<>();
+	private static final Map<Object, List<ObservableDerivation>> theDerivations = new IdentityHashMap<>();
+
+	private static final Map<Object, ObservableDerivation> theDerived = new IdentityHashMap<>();
+	private static final Map<Object, Map<String, ObservableTag>> theTags = new IdentityHashMap<>();
+
+	private static final Map<String, Map<Object, List<ObservableTag>>> theTagged = new HashMap<>();
+	private static final ReentrantReadWriteLock theLock = new ReentrantReadWriteLock();
 
 	/** Enables debugging on the current thread until {@link #endDebug()} is called */
 	public static void startDebug() {
@@ -161,9 +199,191 @@ public final class ObservableDebug {
 		return debug;
 	}
 
+	public static void derived(Object base, Object derived, String derivation, Object function) {
+		derived(new Object[] {base}, derived, derivation, function);
+	}
+
+	public static void derived(Object [] bases, Object derived, String derivation, Object function) {
+		if(!DEBUG_ON)
+			return;
+		Lock lock = theLock.writeLock();
+		lock.lock();
+		try {
+			ObservableDerivation d = new ObservableDerivation(Arrays.asList(bases), derived, derivation, function);
+			if(theDerived.put(derived, d) != null)
+				System.err.println("WARNING! Observable " + derived + " is being reported as a derivation twice.");
+			for(Object base : bases) {
+				List<ObservableDerivation> derivations = theDerivations.get(base);
+				if(derivations == null) {
+					derivations = new ArrayList<>();
+					theDerivations.put(base, derivations);
+				}
+				derivations.add(d);
+			}
+		} finally {
+			lock.unlock();
+		}
+	}
+
+	public static Collection<ObservableDerivation> getDerivations(Object base) {
+		if(!DEBUG_ON)
+			throw new IllegalStateException("Observable debugging is not enabled");
+		Lock lock = theLock.readLock();
+		lock.lock();
+		try {
+			List<ObservableDerivation> derivations = theDerivations.get(base);
+			if(derivations == null)
+				return Collections.EMPTY_LIST;
+			return Collections.unmodifiableCollection(new ArrayList<>(derivations));
+		} finally {
+			lock.unlock();
+		}
+	}
+
+	public static ObservableDerivation getDerived(Object derived) {
+		if(!DEBUG_ON)
+			throw new IllegalStateException("Observable debugging is not enabled");
+		Lock lock = theLock.readLock();
+		lock.lock();
+		try {
+			return theDerived.get(derived);
+		} finally {
+			lock.unlock();
+		}
+	}
+
+	public static void label(Object observable, String label) {
+		if(!DEBUG_ON)
+			return;
+		Lock lock = theLock.writeLock();
+		lock.lock();
+		try {
+			ObservableTag tag = new ObservableTag(observable, label, null);
+			Map<String, ObservableTag> tags = theTags.get(observable);
+			if(tags == null) {
+				tags = new HashMap<>();
+				theTags.put(observable, tags);
+			}
+			tags.put(label, tag);
+		} finally {
+			lock.unlock();
+		}
+	}
+
+	public Collection<String> getLabels(Object observable) {
+		if(!DEBUG_ON)
+			throw new IllegalStateException("Observable debugging is not enabled");
+		Lock lock = theLock.readLock();
+		lock.lock();
+		try {
+			Map<String, ObservableTag> tags = theTags.get(observable);
+			if(tags == null)
+				return Collections.EMPTY_LIST;
+			HashSet<String> ret = new HashSet<>();
+			for(ObservableTag tag : tags.values())
+				if(tag.value == null)
+					ret.add(tag.tag);
+			return Collections.unmodifiableCollection(ret);
+		} finally {
+			lock.unlock();
+		}
+	}
+
+	public static void tag(Object observable, String tag, Object value) {
+		if(!DEBUG_ON)
+			return;
+		Lock lock = theLock.writeLock();
+		lock.lock();
+		try {
+			ObservableTag obTag = new ObservableTag(observable, tag, value);
+			Map<String, ObservableTag> tags = theTags.get(observable);
+			if(tags == null) {
+				tags = new HashMap<>();
+				theTags.put(observable, tags);
+			}
+			if(tags.put(tag, obTag) != null)
+				System.err.println("WARNING! Observable " + observable + " is being tagged with \"" + tag + "\" twice: " + value);
+			Map<Object, List<ObservableTag>> tagged = theTagged.get(tag);
+			if(tagged == null) {
+				tagged = new IdentityHashMap<>();
+				theTagged.put(tag, tagged);
+			}
+			List<ObservableTag> taggedList = tagged.get(value);
+			if(taggedList == null) {
+				taggedList = new ArrayList<>();
+				tagged.put(value, taggedList);
+			}
+			taggedList.add(obTag);
+		} finally {
+			lock.unlock();
+		}
+	}
+
+	public Map<String, Object> getTags(Object observable) {
+		if(!DEBUG_ON)
+			throw new IllegalStateException("Observable debugging is not enabled");
+		Lock lock = theLock.readLock();
+		lock.lock();
+		try {
+			Map<String, ObservableTag> tags = theTags.get(observable);
+			if(tags == null)
+				return Collections.EMPTY_MAP;
+			Map<String, Object> ret = new HashMap<>();
+			for(ObservableTag tag : tags.values())
+				if(tag.value != null)
+					ret.put(tag.tag, tag.value);
+			return Collections.unmodifiableMap(ret);
+		} finally {
+			lock.unlock();
+		}
+	}
+
+	public Map<Object, Collection<Object>> getTagged(String tag) {
+		if(!DEBUG_ON)
+			throw new IllegalStateException("Observable debugging is not enabled");
+		Lock lock = theLock.readLock();
+		lock.lock();
+		try {
+			Map<Object, List<ObservableTag>> tagged = theTagged.get(tag);
+			if(tagged == null)
+				return Collections.EMPTY_MAP;
+			Map<Object, Collection<Object>> ret = new IdentityHashMap<>();
+			for(Map.Entry<Object, List<ObservableTag>> entry : tagged.entrySet()) {
+				List<Object> retValue = new ArrayList<>();
+				ret.put(entry.getKey(), Collections.unmodifiableList(retValue));
+				for(ObservableTag obTag : entry.getValue())
+					retValue.add(obTag.tagged);
+			}
+			return Collections.unmodifiableMap(ret);
+		} finally {
+			lock.unlock();
+		}
+	}
+
+	public Collection<Object> getTagged(String tag, Object value) {
+		if(!DEBUG_ON)
+			throw new IllegalStateException("Observable debugging is not enabled");
+		Lock lock = theLock.readLock();
+		lock.lock();
+		try {
+			Map<Object, List<ObservableTag>> tagged = theTagged.get(tag);
+			if(tagged == null)
+				return Collections.EMPTY_LIST;
+			List<ObservableTag> tags = tagged.get(value);
+			if(tags == null)
+				return Collections.EMPTY_LIST;
+			List<Object> ret = new ArrayList<>();
+			for(ObservableTag obTag : tags)
+				ret.add(obTag.tagged);
+			return Collections.unmodifiableCollection(ret);
+		} finally {
+			lock.unlock();
+		}
+	}
+
 	/**
 	 * To be called from the {@link Observer#onNext(Object)} method
-	 * 
+	 *
 	 * @param obs The observable firing the value
 	 * @param name The name of the observable to print
 	 * @param msg The message to print
@@ -180,7 +400,7 @@ public final class ObservableDebug {
 
 	/**
 	 * To be called from the {@link Observer#onCompleted(Object)} method
-	 * 
+	 *
 	 * @param obs The observable firing the value
 	 * @param name The name of the observable to print
 	 * @param msg The message to print
@@ -197,7 +417,7 @@ public final class ObservableDebug {
 
 	/**
 	 * To be called from the {@link Observable#internalSubscribe(Observer)} method
-	 * 
+	 *
 	 * @param obs The observable being subscribed to
 	 * @param name The name of the observable to print
 	 * @param msg The message to print
@@ -214,7 +434,7 @@ public final class ObservableDebug {
 
 	/**
 	 * To be called from the {@link Runnable#run()} method of the Runnable returned from the {@link Observable#internalSubscribe(Observer)}
-	 * 
+	 *
 	 * @param obs The observable being unsubscribed from
 	 * @param name The name of the observable to print
 	 * @param msg The message to print
