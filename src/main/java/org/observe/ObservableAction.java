db115ba54cad8d772f9fe832857e2b748b16291b
Updownquark <Updownquark@gmail.com>
2016-07-20 16:30:11 -0600

    Added ObservableAction.flatten

--- src/main/java/org/observe/ObservableAction.java
+++ src/main/java/org/observe/ObservableAction.java
@@ -10,4 +10,30 @@ public interface ObservableAction {
 
 	/** @return An observable whose value reports null if this value can be set directly, or a string describing why it cannot */
 	ObservableValue<String> isEnabled();
+
+	static ObservableAction flatten(ObservableValue<? extends ObservableAction> wrapper) {
+		return new FlattenedObservableAction(wrapper);
+	}
+
+	class FlattenedObservableAction implements ObservableAction {
+		private final ObservableValue<? extends ObservableAction> theWrapper;
+
+		public FlattenedObservableAction(ObservableValue<? extends ObservableAction> wrapper) {
+			theWrapper = wrapper;
+		}
+
+		@Override
+		public void act(Object cause) throws IllegalStateException {
+			ObservableAction wrapped = theWrapper.get();
+			if (wrapped != null)
+				wrapped.act(cause);
+			else
+				throw new IllegalStateException("This wrapper (" + theWrapper + ") is empty");
+		}
+
+		@Override
+		public ObservableValue<String> isEnabled() {
+			return ObservableValue.flatten(theWrapper.mapV(action -> action.isEnabled()));
+		}
+	}
 }
