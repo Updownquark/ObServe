c2809ea4b59a0043c4173c32e464f36b0f4abfda
Updownquark <Updownquark@gmail.com>
2017-03-17 15:57:09 -0600

    Added more swing support

--- src/main/java/org/observe/util/swing/ObservableComboBoxModel.java
+++ src/main/java/org/observe/util/swing/ObservableComboBoxModel.java
@@ -1,7 +1,14 @@
 package org.observe.util.swing;
 
-import javax.swing.ComboBoxModel;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
+import java.util.function.Consumer;
 
+import javax.swing.ComboBoxModel;
+import javax.swing.JComboBox;
+
+import org.observe.SettableValue;
+import org.observe.Subscription;
 import org.observe.collect.ObservableList;
 import org.observe.collect.ObservableOrderedCollection;
 
@@ -30,5 +37,50 @@ public class ObservableComboBoxModel<E> extends ObservableListModel<E> implement
 	@Override
 	public E getSelectedItem() {
 		return theSelectedValue;
+	}
+
+	public static <T> Subscription comboFor(JComboBox<T> comboBox, String descrip, ObservableOrderedCollection<T> availableValues,
+		SettableValue<? super T> selected) {
+		ObservableComboBoxModel<T> comboModel = new ObservableComboBoxModel<>(availableValues);
+		comboBox.setModel(comboModel);
+		boolean[] callbackLock = new boolean[1];
+		Consumer<String> checkEnabled = enabled -> {
+			if (enabled == null) {
+				enabled = selected.isAcceptable((T) comboBox.getSelectedItem());
+			}
+			comboBox.setEnabled(enabled == null);
+			comboBox.setToolTipText(enabled == null ? descrip : enabled);
+		};
+		ItemListener itemListener = evt -> {
+			if (evt.getStateChange() != ItemEvent.SELECTED)
+				return;
+			if (!callbackLock[0]) {
+				callbackLock[0] = true;
+				try {
+					selected.set((T) evt.getItem(), evt);
+				} finally {
+					callbackLock[0] = false;
+				}
+			} else
+				checkEnabled.accept(selected.isEnabled().get());
+		};
+		comboBox.addItemListener(itemListener);
+		Subscription valueSub = selected.act(evt -> {
+			if (!callbackLock[0]) {
+				callbackLock[0] = true;
+				try {
+					comboBox.setSelectedItem(evt.getValue());
+				} finally {
+					callbackLock[0] = false;
+				}
+			}
+			checkEnabled.accept(selected.isEnabled().get());
+		});
+		Subscription enabledSub = selected.isEnabled().act(evt -> checkEnabled.accept(evt.getValue()));
+		return () -> {
+			valueSub.unsubscribe();
+			enabledSub.unsubscribe();
+			comboBox.removeItemListener(itemListener);
+		};
 	}
 }
--- src/main/java/org/observe/util/swing/ObservableSwingUtils.java
+++ src/main/java/org/observe/util/swing/ObservableSwingUtils.java
@@ -1,12 +1,200 @@
 package org.observe.util.swing;
 
+import java.awt.event.ActionListener;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
+import java.util.function.Consumer;
 
+import javax.swing.JCheckBox;
+import javax.swing.JOptionPane;
+import javax.swing.JSlider;
+import javax.swing.JSpinner;
+import javax.swing.JToggleButton;
+import javax.swing.event.ChangeListener;
+
+import org.observe.SettableValue;
+import org.observe.Subscription;
 import org.qommons.IntList;
 
 /** Utilities for the org.observe.util.swing package */
 public class ObservableSwingUtils {
+	public static Subscription checkFor(JCheckBox checkBox, String descrip, SettableValue<Boolean> selected) {
+		ActionListener action = evt -> {
+			selected.set(checkBox.isSelected(), evt);
+		};
+		checkBox.addActionListener(action);
+		boolean[] callbackLock = new boolean[1];
+		Consumer<String> checkEnabled = enabled -> {
+			if (enabled == null) {
+				enabled = selected.isAcceptable(!checkBox.isSelected());
+			}
+			checkBox.setEnabled(enabled == null);
+			checkBox.setToolTipText(enabled == null ? descrip : enabled);
+		};
+		Subscription valueSub = selected.act(evt -> {
+			if (!callbackLock[0])
+				checkBox.setSelected(evt.getValue());
+			checkEnabled.accept(selected.isEnabled().get());
+		});
+		Subscription enabledSub = selected.isEnabled().act(evt -> checkEnabled.accept(evt.getValue()));
+		return () -> {
+			valueSub.unsubscribe();
+			enabledSub.unsubscribe();
+			checkBox.removeActionListener(action);
+		};
+	}
+
+	public static <T> Subscription togglesFor(JToggleButton[] buttons, T[] options, String[] descrips, SettableValue<T> selected) {
+		ActionListener[] actions = new ActionListener[buttons.length];
+		for (int i = 0; i < buttons.length; i++) {
+			int index = i;
+			actions[i] = evt -> {
+				selected.set(options[index], evt);
+			};
+			buttons[i].addActionListener(actions[i]);
+		}
+		Consumer<String> checkEnabled = enabled -> {
+			for (int i = 0; i < buttons.length; i++) {
+				String enabled_i = enabled;
+				if (enabled_i == null) {
+					enabled_i = selected.isAcceptable(options[i]);
+				}
+				buttons[i].setEnabled(enabled_i == null);
+				if (enabled != null) {
+					buttons[i].setToolTipText(enabled_i);
+				} else if (descrips != null) {
+					buttons[i].setToolTipText(descrips[i]);
+				} else {
+					buttons[i].setToolTipText(null);
+				}
+			}
+		};
+		Subscription selectedSub = selected.act(evt -> {
+			T value = evt.getValue();
+			for (int i = 0; i < options.length; i++) {
+				buttons[i].setSelected(Objects.equals(options[i], value));
+			}
+			checkEnabled.accept(selected.isEnabled().get());
+		});
+		Subscription enabledSub = selected.isEnabled().act(evt -> checkEnabled.accept(evt.getValue()));
+		return () -> {
+			selectedSub.unsubscribe();
+			enabledSub.unsubscribe();
+			for (int i = 0; i < buttons.length; i++)
+				buttons[i].removeActionListener(actions[i]);
+		};
+	}
+
+	public static Subscription intSpinnerFor(JSpinner spinner, String descrip, SettableValue<Integer> value) {
+		return spinnerFor(spinner, descrip, value, true);
+	}
+
+	public static Subscription doubleSpinnerFor(JSpinner spinner, String descrip, SettableValue<Double> value) {
+		return spinnerFor(spinner, descrip, value, false);
+	}
+
+	private static Subscription spinnerFor(JSpinner spinner, String descrip, SettableValue<? extends Number> value, boolean integer) {
+		boolean[] callbackLock = new boolean[1];
+		ChangeListener changeListener = evt -> {
+			if (!callbackLock[0]) {
+				callbackLock[0] = true;
+				try {
+					if (integer) {
+						int newValue = ((Number) spinner.getValue()).intValue();
+						String accept = ((SettableValue<Integer>) value).isAcceptable(newValue);
+						if (accept != null) {
+							JOptionPane.showMessageDialog(spinner.getParent(), accept, "Unacceptable Value", JOptionPane.ERROR_MESSAGE);
+							spinner.setValue(value.get());
+						} else {
+							((SettableValue<Integer>) value).set(newValue, evt);
+						}
+					} else {
+						double newValue = ((Number) spinner.getValue()).doubleValue();
+						String accept = ((SettableValue<Double>) value).isAcceptable(newValue);
+						if (accept != null) {
+							JOptionPane.showMessageDialog(spinner.getParent(), accept, "Unacceptable Value", JOptionPane.ERROR_MESSAGE);
+							spinner.setValue(value.get());
+						} else {
+							((SettableValue<Double>) value).set(newValue, evt);
+						}
+					}
+				} finally {
+					callbackLock[0] = false;
+				}
+			}
+		};
+		spinner.addChangeListener(changeListener);
+
+		Subscription valueSub = value.act(evt -> {
+			if (!callbackLock[0]) {
+				callbackLock[0] = true;
+				try {
+					Number newValue;
+					if (integer)
+						newValue = Integer.valueOf(evt.getValue().intValue());
+					else
+						newValue = Double.valueOf(evt.getValue().doubleValue());
+					spinner.setValue(newValue);
+				} finally {
+					callbackLock[0] = false;
+				}
+			}
+		});
+		Subscription enabledSub = value.isEnabled().act(evt -> {
+			String enabled = evt.getValue();
+			spinner.setEnabled(enabled == null);
+			spinner.setToolTipText(enabled != null ? enabled : descrip);
+		});
+		return () -> {
+			valueSub.unsubscribe();
+			enabledSub.unsubscribe();
+			spinner.removeChangeListener(changeListener);
+		};
+	}
+
+	public static Subscription sliderFor(JSlider slider, String descrip, SettableValue<Integer> value) {
+		boolean[] callbackLock = new boolean[1];
+		ChangeListener changeListener = evt -> {
+			if (!callbackLock[0]) {
+				callbackLock[0] = true;
+				try {
+					int newValue = slider.getValue();
+					String accept = value.isAcceptable(newValue);
+					if (accept != null) {
+						JOptionPane.showMessageDialog(slider.getParent(), accept, "Unacceptable Value", JOptionPane.ERROR_MESSAGE);
+						slider.setValue(value.get());
+					} else {
+						value.set(newValue, evt);
+					}
+				} finally {
+					callbackLock[0] = false;
+				}
+			}
+		};
+		slider.addChangeListener(changeListener);
+		Subscription valueSub = value.act(evt -> {
+			if (!callbackLock[0]) {
+				callbackLock[0] = true;
+				try {
+					slider.setValue(evt.getValue());
+				} finally {
+					callbackLock[0] = false;
+				}
+			}
+		});
+		Subscription enabledSub = value.isEnabled().act(evt -> {
+			String enabled = evt.getValue();
+			slider.setEnabled(enabled == null);
+			slider.setToolTipText(enabled != null ? enabled : descrip);
+		});
+		return () -> {
+			valueSub.unsubscribe();
+			enabledSub.unsubscribe();
+			slider.removeChangeListener(changeListener);
+		};
+	}
+
 	/**
 	 * Sorts a list of ints and returns the start and end of each continuous interval in the list
 	 *
@@ -14,34 +202,34 @@ public class ObservableSwingUtils {
 	 * @param forward Whether to iterate from least to greatest or greatest to least
 	 * @return The intervals, in the form of [start, end] arrays
 	 */
-	public static int [] [] getContinuousIntervals(int [] ints, boolean forward) {
+	public static int[][] getContinuousIntervals(int[] ints, boolean forward) {
 		IntList indexes = new IntList(true, true);
 		indexes.addAll(ints);
 		int start, end;
-		List<int []> ret = new ArrayList<>();
-		if(forward) {
+		List<int[]> ret = new ArrayList<>();
+		if (forward) {
 			start = 0;
 			end = 0;
-			while(end < indexes.size()) {
-				while(end < indexes.size() - 1 && indexes.get(end + 1) == indexes.get(end) + 1) {
+			while (end < indexes.size()) {
+				while (end < indexes.size() - 1 && indexes.get(end + 1) == indexes.get(end) + 1) {
 					end++;
 				}
-				ret.add(new int[] {indexes.get(start), indexes.get(end)});
+				ret.add(new int[] { indexes.get(start), indexes.get(end) });
 				start = end = end + 1;
 			}
 		} else {
 			end = indexes.size() - 1;
 			start = end;
-			while(start >= 0) {
-				while(start > 0 && indexes.get(start - 1) == indexes.get(start) - 1) {
+			while (start >= 0) {
+				while (start > 0 && indexes.get(start - 1) == indexes.get(start) - 1) {
 					start--;
 				}
-				ret.add(new int[] {indexes.get(start), indexes.get(end)});
+				ret.add(new int[] { indexes.get(start), indexes.get(end) });
 				start = end = start - 1;
 			}
 		}
-		int [] [] retArray = new int[ret.size()][];
-		for(int i = 0; i < retArray.length; i++) {
+		int[][] retArray = new int[ret.size()][];
+		for (int i = 0; i < retArray.length; i++) {
 			retArray[i] = ret.get(i);
 		}
 		return retArray;
