1da0f878ff889c37b3724f7f0a4668af749485c4
abutler <abutler@psl.nmsu.edu>
2018-01-02 16:28:40 -0700

    #12: The start of a thought on how to do addAll better

---
+++ src/main/test/org/observe/supertest/CollectionModTransaction.java
@@ -0,0 +1,10 @@
+package org.observe.supertest;
+
+import java.util.List;
+
+public class CollectionModTransaction<E> {
+	private final CollectionModTransaction<E> theParent;
+	private final List<E> theValues;
+
+	public CollectionModTransaction(
+}
