fa5e16b75582e99747e1f6873c43b7e968ae8409
abutler <abutler@psl.nmsu.edu>
2018-01-08 12:39:22 -0700

    Implemented variable mapping (refresh)

--- src/main/test/org/observe/supertest/dev/MappedCollectionLink.java
+++ src/main/test/org/observe/supertest/dev/MappedCollectionLink.java
@@ -1,28 +1,60 @@
 package org.observe.supertest.dev;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
+import org.observe.SimpleSettableValue;
 import org.observe.collect.FlowOptions;
 import org.observe.collect.ObservableCollection.CollectionDataFlow;
 import org.observe.supertest.dev.ObservableChainTester.TestValueType;
-import org.observe.supertest.dev.ObservableChainTester.TypeTransformation;
+import org.qommons.BiTuple;
 import org.qommons.TestHelper;
+import org.qommons.TestHelper.RandomAction;
 import org.qommons.collect.MutableCollectionElement.StdMsg;
 
 public class MappedCollectionLink<E, T> extends AbstractObservableCollectionLink<E, T> {
-	private final TypeTransformation<E, T> theMap;
+	private final SimpleSettableValue<TypeTransformation<E, T>> theMapValue;
+	private final boolean isMapVariable;
+	private TypeTransformation<E, T> theMap;
 	private final FlowOptions.MapDef<E, T> theOptions;
 
 	public MappedCollectionLink(ObservableCollectionChainLink<?, E> parent, TestValueType type, CollectionDataFlow<?, ?, T> flow,
-		TestHelper helper, TypeTransformation<E, T> map, FlowOptions.MapDef<E, T> options) {
-		super(parent, type, flow, helper, false);
-		theMap = map;
+		TestHelper helper, boolean checkRemovedValues, SimpleSettableValue<TypeTransformation<E, T>> map, boolean variableMap,
+		FlowOptions.MapDef<E, T> options) {
+		super(parent, type, flow, helper, false, checkRemovedValues);
+		theMapValue = map;
+		isMapVariable = variableMap;
+		theMap = map.get();
 		theOptions = options;
 
 		for (E src : getParent().getCollection())
 			getExpected().add(theMap.map(src));
+	}
+
+	@Override
+	protected void addExtraActions(RandomAction action) {
+		super.addExtraActions(action);
+		if(isMapVariable){
+			action.or(1, () -> {
+				TypeTransformation<E, T> newMap = transform(getParent().getTestType(), getTestType(), action.getHelper(),
+					theOptions.isManyToOne());
+				TypeTransformation<E, T> oldMap = theMap;
+				if (action.getHelper().isReproducing())
+					System.out.println("Map change from " + oldMap + " to " + newMap);
+				theMap = newMap;
+				theMapValue.set(newMap, null);
+				for (int i = 0; i < getParent().getExpected().size(); i++) {
+					E src = getParent().getExpected().get(i);
+					T newValue = newMap.map(src);
+					set(i, newValue, action.getHelper(), true);
+				}
+			});
+		}
 	}
 
 	@Override
@@ -137,5 +169,252 @@ public class MappedCollectionLink<E, T> extends AbstractObservableCollectionLink
 	@Override
 	public String toString() {
 		return "mapped(" + theMap + ")";
+	}
+
+	public interface TypeTransformation<E, T> {
+		T map(E source);
+
+		E reverse(T mapped);
+
+		boolean isManyToOne();
+
+		boolean isOneToMany();
+
+		String reverseName();
+
+		default TypeTransformation<T, E> reverse() {
+			TypeTransformation<E, T> outer = this;
+			return new TypeTransformation<T, E>() {
+				@Override
+				public E map(T source) {
+					return outer.reverse(source);
+				}
+
+				@Override
+				public T reverse(E mapped) {
+					return outer.map(mapped);
+				}
+
+				@Override
+				public boolean isManyToOne() {
+					return outer.isOneToMany();
+				}
+
+				@Override
+				public boolean isOneToMany() {
+					return outer.isManyToOne();
+				}
+
+				@Override
+				public TypeTransformation<E, T> reverse() {
+					return outer;
+				}
+
+				@Override
+				public String reverseName() {
+					return outer.toString();
+				}
+
+				@Override
+				public String toString() {
+					return outer.reverseName();
+				}
+			};
+		}
+	}
+
+	public static <E, T> TypeTransformation<E, T> transform(TestValueType type1, TestValueType type2, TestHelper helper) {
+		List<? extends TypeTransformation<?, ?>> transforms = TYPE_TRANSFORMATIONS.get(new BiTuple<>(type1, type2));
+		return (TypeTransformation<E, T>) transforms.get(helper.getInt(0, transforms.size()));
+	}
+
+	public static <E, T> TypeTransformation<E, T> transform(TestValueType type1, TestValueType type2, TestHelper helper,
+		boolean allowManyToOne) {
+		List<? extends TypeTransformation<?, ?>> transforms = TYPE_TRANSFORMATIONS.get(new BiTuple<>(type1, type2));
+		if (!allowManyToOne)
+			transforms = transforms.stream().filter(t -> !t.isManyToOne()).collect(Collectors.toList());
+		return (TypeTransformation<E, T>) transforms.get(helper.getInt(0, transforms.size()));
+	}
+
+	private static <E> TypeTransformation<E, E> identity() {
+		return new TypeTransformation<E, E>() {
+			@Override
+			public E map(E source) {
+				return source;
+			}
+
+			@Override
+			public E reverse(E mapped) {
+				return mapped;
+			}
+
+			@Override
+			public boolean isManyToOne() {
+				return false;
+			}
+
+			@Override
+			public boolean isOneToMany() {
+				return false;
+			}
+
+			@Override
+			public String reverseName() {
+				return "identity";
+			}
+
+			@Override
+			public String toString() {
+				return "identity";
+			}
+		};
+	}
+
+	private static <E, T> TypeTransformation<E, T> transform(Function<E, T> map, Function<T, E> reverse, boolean manyToOne,
+		boolean oneToMany, String name, String reverseName) {
+		return new TypeTransformation<E, T>() {
+			@Override
+			public T map(E source) {
+				return map.apply(source);
+			}
+
+			@Override
+			public E reverse(T mapped) {
+				return reverse.apply(mapped);
+			}
+
+			@Override
+			public boolean isManyToOne() {
+				return manyToOne;
+			}
+
+			@Override
+			public boolean isOneToMany() {
+				return oneToMany;
+			}
+
+			@Override
+			public String reverseName() {
+				return reverseName;
+			}
+
+			@Override
+			public String toString() {
+				return name;
+			}
+		};
+	}
+
+	private static <E, T> List<TypeTransformation<E, T>> asList(TypeTransformation<E, T>... transforms) {
+		return Arrays.asList(transforms);
+	}
+
+	private static String reverse(String s) {
+		char[] c = s.toCharArray();
+		int start = 0;
+		if (c.length > 0 && c[start] == '-')
+			start++;
+		for (int i = start; i <= c.length / 2; i++) {
+			char temp = c[i];
+			int opposite = c.length - i - 1;
+			c[i] = c[opposite];
+			c[opposite] = temp;
+		}
+		return new String(c);
+	}
+
+	private static final Map<BiTuple<TestValueType, TestValueType>, List<? extends TypeTransformation<?, ?>>> TYPE_TRANSFORMATIONS;
+	static {
+		TYPE_TRANSFORMATIONS = new HashMap<>();
+		for (TestValueType type1 : TestValueType.values()) {
+			for (TestValueType type2 : TestValueType.values()) {
+				switch (type1) {
+				case INT:
+					switch (type2) {
+					case INT: {
+						List<TypeTransformation<Integer, Integer>> transforms = asList(//
+							identity(), //
+							transform(i -> i + 5, i -> i - 5, false, false, "+5", "-5"), //
+							transform(i -> i - 5, i -> i + 5, false, false, "-5", "+5"), //
+							transform(i -> -i, i -> -i, false, false, "-", "-"));
+						TYPE_TRANSFORMATIONS.put(new BiTuple<>(type1, type2), transforms);
+						break;
+					}
+					case DOUBLE: {
+						List<TypeTransformation<Integer, Double>> transforms = asList(//
+							transform(//
+								i -> i * 1.0, //
+								d -> (int) Math.round(d), //
+								false, true, "*1.0", "round()"), //
+							transform(//
+								i -> i * 5.0, //
+								d -> (int) Math.round(d / 5), //
+								false, true, "*5.0", "/5,round"),
+							transform(//
+								i -> i / 5.0, //
+								d -> (int) Math.round(d * 5), //
+								false, true, "/5.0", "*5,round"));
+						TYPE_TRANSFORMATIONS.put(new BiTuple<>(type1, type2), transforms);
+						TYPE_TRANSFORMATIONS.put(new BiTuple<>(type2, type1),
+							transforms.stream().map(t -> t.reverse()).collect(Collectors.toList()));
+						break;
+					}
+					case STRING: {
+						List<TypeTransformation<Integer, String>> transforms = asList(//
+							transform(i -> String.valueOf(i), s -> (int) Math.round(Double.parseDouble(s)), false, false, "toString()",
+								"parseInt"));
+						TYPE_TRANSFORMATIONS.put(new BiTuple<>(type1, type2), transforms);
+						TYPE_TRANSFORMATIONS.put(new BiTuple<>(type2, type1),
+							transforms.stream().map(t -> t.reverse()).collect(Collectors.toList()));
+					}
+					}
+					break;
+				case DOUBLE:
+					switch (type2) {
+					case INT: // Already done above
+						break;
+					case DOUBLE: {
+						List<TypeTransformation<Double, Double>> transforms = asList(//
+							identity(), //
+							transform(d -> d + 5, d -> d - 5, false, false, "+5", "-5"), //
+							transform(d -> d - 5, d -> d + 5, false, false, "-5", "+5"), //
+							transform(d -> d * 5, d -> d / 5, false, false, "*5", "/5"), //
+							transform(d -> d / 5, d -> d * 5, false, false, "/5", "*5"), //
+							transform(d -> -d, d -> -d, false, false, "-", "-"));
+						TYPE_TRANSFORMATIONS.put(new BiTuple<>(type1, type2), transforms);
+						break;
+					}
+					case STRING: {
+						List<TypeTransformation<Double, String>> transforms = asList(//
+							transform(d -> stringValueOf(d), s -> Double.valueOf(s), false, false, "toString()", "parseDouble"));
+						TYPE_TRANSFORMATIONS.put(new BiTuple<>(type1, type2), transforms);
+						TYPE_TRANSFORMATIONS.put(new BiTuple<>(type2, type1),
+							transforms.stream().map(t -> t.reverse()).collect(Collectors.toList()));
+						break;
+					}
+					}
+					break;
+				case STRING:
+					switch (type2) {
+					case INT:
+					case DOUBLE:
+						break;
+					case STRING: {
+						List<TypeTransformation<String, String>> transforms = asList(//
+							identity(), transform(s -> reverse(s), s -> reverse(s), false, false, "reverse", "reverse"));
+						TYPE_TRANSFORMATIONS.put(new BiTuple<>(type1, type2), transforms);
+						break;
+					}
+					}
+				}
+			}
+		}
+	}
+
+	private static String stringValueOf(double d) {
+		String str = String.valueOf(d);
+		if (str.endsWith(".0"))
+			str = str.substring(0, str.length() - 2);
+		return str;
 	}
 }
--- src/main/test/org/observe/supertest/dev/ObservableChainTester.java
+++ src/main/test/org/observe/supertest/dev/ObservableChainTester.java
@@ -9,18 +9,15 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.function.Function;
-import java.util.stream.Collectors;
 
 import org.junit.Test;
 import org.observe.collect.DefaultObservableCollection;
 import org.observe.collect.FlowOptions;
-import org.qommons.BiTuple;
 import org.qommons.QommonsUtils;
 import org.qommons.TestHelper;
 import org.qommons.TestHelper.Testable;
 import org.qommons.Transaction;
 import org.qommons.collect.BetterList;
-import org.qommons.debug.Debug;
 import org.qommons.tree.BetterTreeList;
 import org.qommons.tree.BetterTreeSet;
 
@@ -42,157 +39,13 @@ public class ObservableChainTester implements Testable {
 		}
 	}
 
-	public interface TypeTransformation<E, T> {
-		T map(E source);
-
-		E reverse(T mapped);
-
-		boolean isManyToOne();
-
-		boolean isOneToMany();
-
-		String reverseName();
-
-		default TypeTransformation<T, E> reverse() {
-			TypeTransformation<E, T> outer = this;
-			return new TypeTransformation<T, E>() {
-				@Override
-				public E map(T source) {
-					return outer.reverse(source);
-				}
-
-				@Override
-				public T reverse(E mapped) {
-					return outer.map(mapped);
-				}
-
-				@Override
-				public boolean isManyToOne() {
-					return outer.isOneToMany();
-				}
-
-				@Override
-				public boolean isOneToMany() {
-					return outer.isManyToOne();
-				}
-
-				@Override
-				public TypeTransformation<E, T> reverse() {
-					return outer;
-				}
-
-				@Override
-				public String reverseName() {
-					return outer.toString();
-				}
-
-				@Override
-				public String toString() {
-					return outer.reverseName();
-				}
-			};
-		}
-	}
-
 	public static TestValueType nextType(TestHelper helper) {
 		return TestValueType.values()[helper.getInt(0, TestValueType.values().length)];
 	}
 
-	public static <E, T> TypeTransformation<E, T> transform(TestValueType type1, TestValueType type2, TestHelper helper) {
-		List<? extends TypeTransformation<?, ?>> transforms = TYPE_TRANSFORMATIONS.get(new BiTuple<>(type1, type2));
-		return (TypeTransformation<E, T>) transforms.get(helper.getInt(0, transforms.size()));
-	}
-
-	private static <E> TypeTransformation<E, E> identity() {
-		return new TypeTransformation<E, E>() {
-			@Override
-			public E map(E source) {
-				return source;
-			}
-
-			@Override
-			public E reverse(E mapped) {
-				return mapped;
-			}
-
-			@Override
-			public boolean isManyToOne() {
-				return false;
-			}
-
-			@Override
-			public boolean isOneToMany() {
-				return false;
-			}
-
-			@Override
-			public String reverseName() {
-				return "identity";
-			}
-
-			@Override
-			public String toString() {
-				return "identity";
-			}
-		};
-	}
-
-	private static <E, T> TypeTransformation<E, T> transform(Function<E, T> map, Function<T, E> reverse, boolean manyToOne,
-		boolean oneToMany, String name, String reverseName) {
-		return new TypeTransformation<E, T>() {
-			@Override
-			public T map(E source) {
-				return map.apply(source);
-			}
-
-			@Override
-			public E reverse(T mapped) {
-				return reverse.apply(mapped);
-			}
-
-			@Override
-			public boolean isManyToOne() {
-				return manyToOne;
-			}
-
-			@Override
-			public boolean isOneToMany() {
-				return oneToMany;
-			}
-
-			@Override
-			public String reverseName() {
-				return reverseName;
-			}
-
-			@Override
-			public String toString() {
-				return name;
-			}
-		};
-	}
-
-	private static <E, T> List<TypeTransformation<E, T>> asList(TypeTransformation<E, T>... transforms) {
-		return Arrays.asList(transforms);
-	}
-
-	private static String reverse(String s) {
-		char[] c = s.toCharArray();
-		int start = 0;
-		if (c.length > 0 && c[start] == '-')
-			start++;
-		for (int i = start; i <= c.length / 2; i++) {
-			char temp = c[i];
-			int opposite = c.length - i - 1;
-			c[i] = c[opposite];
-			c[opposite] = temp;
-		}
-		return new String(c);
-	}
 	private static final int MAX_VALUE = 1000;
 	static final Map<TestValueType, Function<TestHelper, ?>> SUPPLIERS;
 	private static final Map<TestValueType, List<? extends Comparator<?>>> COMPARATORS;
-	private static final Map<BiTuple<TestValueType, TestValueType>, List<? extends TypeTransformation<?, ?>>> TYPE_TRANSFORMATIONS;
 	static {
 		SUPPLIERS = new HashMap<>();
 		COMPARATORS = new HashMap<>();
@@ -209,91 +62,6 @@ public class ObservableChainTester implements Testable {
 			case STRING:
 				SUPPLIERS.put(type, helper -> String.valueOf(helper.getInt(0, MAX_VALUE)));
 				COMPARATORS.put(type, Arrays.asList(String::compareTo, ((Comparator<String>) String::compareTo).reversed()));
-			}
-		}
-
-		TYPE_TRANSFORMATIONS = new HashMap<>();
-		for (TestValueType type1 : TestValueType.values()) {
-			for (TestValueType type2 : TestValueType.values()) {
-				switch (type1) {
-				case INT:
-					switch (type2) {
-					case INT: {
-						List<TypeTransformation<Integer, Integer>> transforms = asList(//
-							identity(), //
-							transform(i -> i + 5, i -> i - 5, false, false, "+5", "-5"), //
-							transform(i -> i - 5, i -> i + 5, false, false, "-5", "+5"), //
-							transform(i -> -i, i -> -i, false, false, "-", "-"));
-						TYPE_TRANSFORMATIONS.put(new BiTuple<>(type1, type2), transforms);
-						break;
-					}
-					case DOUBLE: {
-						List<TypeTransformation<Integer, Double>> transforms = asList(//
-							transform(//
-								i -> i * 1.0, //
-								d -> (int) Math.round(d), //
-								false, true, "*1.0", "round()"), //
-							transform(//
-								i -> i * 5.0, //
-								d -> (int) Math.round(d / 5), //
-								false, true, "*5.0", "/5,round"),
-							transform(//
-								i -> i / 5.0, //
-								d -> (int) Math.round(d * 5), //
-								false, true, "/5.0", "*5,round"));
-						TYPE_TRANSFORMATIONS.put(new BiTuple<>(type1, type2), transforms);
-						TYPE_TRANSFORMATIONS.put(new BiTuple<>(type2, type1),
-							transforms.stream().map(t -> t.reverse()).collect(Collectors.toList()));
-						break;
-					}
-					case STRING: {
-						List<TypeTransformation<Integer, String>> transforms = asList(//
-							transform(i -> String.valueOf(i), s -> (int) Math.round(Double.parseDouble(s)), false, false, "toString()",
-								"parseInt"));
-						TYPE_TRANSFORMATIONS.put(new BiTuple<>(type1, type2), transforms);
-						TYPE_TRANSFORMATIONS.put(new BiTuple<>(type2, type1),
-							transforms.stream().map(t -> t.reverse()).collect(Collectors.toList()));
-					}
-					}
-					break;
-				case DOUBLE:
-					switch (type2) {
-					case INT: // Already done above
-						break;
-					case DOUBLE: {
-						List<TypeTransformation<Double, Double>> transforms = asList(//
-							identity(), //
-							transform(d -> d + 5, d -> d - 5, false, false, "+5", "-5"), //
-							transform(d -> d - 5, d -> d + 5, false, false, "-5", "+5"), //
-							transform(d -> d * 5, d -> d / 5, false, false, "*5", "/5"), //
-							transform(d -> d / 5, d -> d * 5, false, false, "/5", "*5"), //
-							transform(d -> -d, d -> -d, false, false, "-", "-"));
-						TYPE_TRANSFORMATIONS.put(new BiTuple<>(type1, type2), transforms);
-						break;
-					}
-					case STRING: {
-						List<TypeTransformation<Double, String>> transforms = asList(//
-							transform(d -> stringValueOf(d), s -> Double.valueOf(s), false, false, "toString()", "parseDouble"));
-						TYPE_TRANSFORMATIONS.put(new BiTuple<>(type1, type2), transforms);
-						TYPE_TRANSFORMATIONS.put(new BiTuple<>(type2, type1),
-							transforms.stream().map(t -> t.reverse()).collect(Collectors.toList()));
-						break;
-					}
-					}
-					break;
-				case STRING:
-					switch (type2) {
-					case INT:
-					case DOUBLE:
-						break;
-					case STRING: {
-						List<TypeTransformation<String, String>> transforms = asList(//
-							identity(), transform(s -> reverse(s), s -> reverse(s), false, false, "reverse", "reverse"));
-						TYPE_TRANSFORMATIONS.put(new BiTuple<>(type1, type2), transforms);
-						break;
-					}
-					}
-				}
 			}
 		}
 	}
@@ -415,11 +183,11 @@ public class ObservableChainTester implements Testable {
 					String preValue = toString();
 					if (helper.isReproducing())
 						System.out.print("\tMod " + (transactionTri + 1) + ": ");
-					Debug.d().debugIf(tri == 31 && transactionTri == 3);
 					try {
 						targetLink.tryModify(helper);
 					} catch (RuntimeException | Error e) {
 						System.err.println("Chain is " + printChain());
+						System.err.println("Link " + linkIndex);
 						System.err.println("Error on transaction " + (tri + 1) + ", mod " + (transactionTri + 1) + " after "
 							+ (modifications + transactionTri) + " successful modifications");
 						System.err.println("Pre-faiure values:\n" + preValue);
@@ -431,6 +199,7 @@ public class ObservableChainTester implements Testable {
 							theChain.get(failedLink).check(!useTransaction);
 					} catch (Error e) {
 						System.err.println("Chain is " + printChain());
+						System.err.println("Link " + linkIndex);
 						System.err.println("Integrity check failure on link " + failedLink + " after "
 							+ (modifications + transactionTri + 1) + " modifications in " + (tri + 1) + " transactions");
 						System.err.println("Pre-faiure values:\n" + preValue);
@@ -443,6 +212,7 @@ public class ObservableChainTester implements Testable {
 			} catch (RuntimeException | Error e) {
 				if (finished) {
 					System.err.println("Chain is " + printChain());
+					System.err.println("Link " + linkIndex);
 					System.err.println("Error closing transaction " + tri + " after " + modifications + " successful modifications");
 				}
 				throw e;
@@ -454,6 +224,7 @@ public class ObservableChainTester implements Testable {
 					theChain.get(failedLink).check(true);
 			} catch (Error e) {
 				System.err.println("Chain is " + printChain());
+				System.err.println("Link " + linkIndex);
 				System.err.println(
 					"Integrity check failure on transaction close on link " + failedLink + " after " + modifications + " modifications");
 				throw e;
@@ -462,7 +233,7 @@ public class ObservableChainTester implements Testable {
 	}
 
 	public String printChain() {
-		StringBuilder s = new StringBuilder(theChain.size()).append(':');
+		StringBuilder s = new StringBuilder().append(theChain.size()).append(':');
 		for (int i = 0; i < theChain.size(); i++) {
 			if (i > 0)
 				s.append("->");
