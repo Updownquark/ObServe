bb5379589751699bde7768fd5c85676daf514a52
abutler <abutler@psl.nmsu.edu>
2019-08-29 16:24:51 -0600

    Added padding to JustifiedBoxLayout.  Not 100% sure it works.

--- src/main/java/org/observe/util/swing/JustifiedBoxLayout.java
+++ src/main/java/org/observe/util/swing/JustifiedBoxLayout.java
@@ -26,8 +26,9 @@ public class JustifiedBoxLayout implements LayoutManager2 {
 	private final boolean isVertical;
 
 	private Alignment theMainAlign;
-
 	private Alignment theCrossAlign;
+
+	private int thePadding;
 
 	/**
 	 * @param vertical Whether the layout will align components in a column or a row
@@ -36,6 +37,23 @@ public class JustifiedBoxLayout implements LayoutManager2 {
 		isVertical = vertical;
 		theMainAlign = Alignment.LEADING;
 		theCrossAlign = Alignment.JUSTIFIED;
+		thePadding = 2;
+	}
+
+	public boolean isVertical() {
+		return isVertical;
+	}
+
+	public Alignment getMainAlignment() {
+		return theMainAlign;
+	}
+
+	public Alignment getCrossAlignment() {
+		return theCrossAlign;
+	}
+
+	public int getPadding() {
+		return thePadding;
 	}
 
 	/**
@@ -56,20 +74,70 @@ public class JustifiedBoxLayout implements LayoutManager2 {
 		return this;
 	}
 
+	/**
+	 * Sets this layout's {@link #setMainAlignment(Alignment) main alignment} to {@link Alignment#JUSTIFIED}
+	 *
+	 * @return This layout
+	 */
+	public JustifiedBoxLayout mainJustified() {
+		return setMainAlignment(Alignment.JUSTIFIED);
+	}
+
+	/**
+	 * Sets this layout's {@link #setMainAlignment(Alignment) main alignment} to {@link Alignment#CENTER}
+	 *
+	 * @return This layout
+	 */
+	public JustifiedBoxLayout mainCenter() {
+		return setMainAlignment(Alignment.CENTER);
+	}
+
+	/**
+	 * Sets this layout's {@link #setCrossAlignment(Alignment) cross alignment} to {@link Alignment#JUSTIFIED}
+	 *
+	 * @return This layout
+	 */
+	public JustifiedBoxLayout crossJustified() {
+		return setCrossAlignment(Alignment.JUSTIFIED);
+	}
+
+	/**
+	 * Sets this layout's {@link #setCrossAlignment(Alignment) cross alignment} to {@link Alignment#CENTER}
+	 *
+	 * @return This layout
+	 */
+	public JustifiedBoxLayout crossCenter() {
+		return setCrossAlignment(Alignment.CENTER);
+	}
+
+	/**
+	 * Sets the padding between components that this layout will use
+	 *
+	 * @param padding The padding
+	 * @return This layout
+	 */
+	public JustifiedBoxLayout setPadding(int padding) {
+		thePadding = padding;
+		return this;
+	}
+
 	@Override
 	public Dimension preferredLayoutSize(Container parent) {
 		int main = 0;
 		int cross = 0;
+		boolean first = true;
 		for (Component comp : parent.getComponents()) {
-			if (!comp.isVisible()) {
+			if (!comp.isVisible())
 				continue;
-			}
+			if (first)
+				first = false;
+			else
+				main += thePadding;
 			Dimension pref = comp.getPreferredSize();
 			main += getMain(pref);
 			int compCross = getCross(pref);
-			if (compCross > cross) {
+			if (compCross > cross)
 				cross = compCross;
-			}
 		}
 		Insets insets = parent.getInsets();
 		int hIns = insets.left + insets.right;
@@ -81,17 +149,20 @@ public class JustifiedBoxLayout implements LayoutManager2 {
 	public Dimension minimumLayoutSize(Container parent) {
 		int main = 0;
 		int cross = 0;
+		boolean first = true;
 		for (Component comp : parent.getComponents()) {
-			if (!comp.isVisible()) {
+			if (!comp.isVisible())
 				continue;
-			}
+			if (first)
+				first = false;
+			else
+				main += thePadding;
 			Dimension min = comp.getMinimumSize();
 			Dimension pref = comp.getPreferredSize();
 			main += getMain(pref);
 			int compCross = getCross(min);
-			if (compCross > cross) {
+			if (compCross > cross)
 				cross = compCross;
-			}
 		}
 		Insets insets = parent.getInsets();
 		int hIns = insets.left + insets.right;
@@ -103,17 +174,20 @@ public class JustifiedBoxLayout implements LayoutManager2 {
 	public Dimension maximumLayoutSize(Container parent) {
 		int main = 0;
 		int cross = 0;
+		boolean first = true;
 		for (Component comp : parent.getComponents()) {
-			if (!comp.isVisible()) {
+			if (!comp.isVisible())
 				continue;
-			}
+			if (first)
+				first = false;
+			else
+				main += thePadding;
 			Dimension max = comp.getMaximumSize();
 			Dimension pref = comp.getPreferredSize();
 			main += getMain(pref);
 			int compCross = getCross(max);
-			if (compCross > cross) {
+			if (compCross > cross)
 				cross = compCross;
-			}
 		}
 		Insets insets = parent.getInsets();
 		int hIns = insets.left + insets.right;
@@ -126,10 +200,14 @@ public class JustifiedBoxLayout implements LayoutManager2 {
 		Insets insets = parent.getInsets();
 		int compCount = 0;
 		int totalLength = isVertical ? insets.top + insets.bottom : insets.left + insets.right;
+		boolean first = true;
 		for (Component comp : parent.getComponents()) {
-			if (!comp.isVisible()) {
+			if (!comp.isVisible())
 				continue;
-			}
+			if (first)
+				first = false;
+			else
+				totalLength += thePadding;
 			compCount++;
 			totalLength += getMain(comp.getPreferredSize());
 		}
@@ -154,14 +232,12 @@ public class JustifiedBoxLayout implements LayoutManager2 {
 			stretch = getMain(parent.getSize()) * 1.0f / totalLength;
 			break;
 		}
-		if (pos < margin) {
+		if (pos < margin)
 			pos = margin;
-		}
 		Rectangle bounds = new Rectangle();
 		for (Component comp : parent.getComponents()) {
-			if (!comp.isVisible()) {
+			if (!comp.isVisible())
 				continue;
-			}
 			Dimension ps = comp.getPreferredSize();
 			int main = getMain(ps);
 			int cross = getCross(ps);
@@ -184,6 +260,7 @@ public class JustifiedBoxLayout implements LayoutManager2 {
 				break;
 			}
 			comp.setBounds(bounds);
+			pos += thePadding;
 		}
 	}
 
