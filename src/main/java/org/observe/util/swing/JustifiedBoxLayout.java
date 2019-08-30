efad2faaa7156b0f069697920a112f73a066e9a8
abutler <abutler@psl.nmsu.edu>
2019-08-30 16:25:21 -0600

    Some big enhancements to JustifiedBoxLayout's justified behavior

--- src/main/java/org/observe/util/swing/JustifiedBoxLayout.java
+++ src/main/java/org/observe/util/swing/JustifiedBoxLayout.java
@@ -6,6 +6,8 @@ import java.awt.Dimension;
 import java.awt.Insets;
 import java.awt.LayoutManager2;
 import java.awt.Rectangle;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * A very simple layout that lays out components in a column or a row, with support for 4 different {@link Alignment alignments} in each
@@ -27,6 +29,7 @@ public class JustifiedBoxLayout implements LayoutManager2 {
 
 	private Alignment theMainAlign;
 	private Alignment theCrossAlign;
+	private boolean isStretchingEqual;
 
 	private int thePadding;
 
@@ -54,6 +57,14 @@ public class JustifiedBoxLayout implements LayoutManager2 {
 
 	public int getPadding() {
 		return thePadding;
+	}
+
+	/**
+	 * @return Whether this layout stretches its components equally instead of favoring more flexible components (only with
+	 *         {@link Alignment#JUSTIFIED justified} {@link #setMainAlignment(Alignment) main alignment})
+	 */
+	public boolean isStretchingEqual() {
+		return isStretchingEqual;
 	}
 
 	/**
@@ -118,6 +129,16 @@ public class JustifiedBoxLayout implements LayoutManager2 {
 	 */
 	public JustifiedBoxLayout setPadding(int padding) {
 		thePadding = padding;
+		return this;
+	}
+
+	/**
+	 * @param stretchEqual Whether this layout should stretch its components equally instead of favoring more flexible components (only with
+	 *        {@link Alignment#JUSTIFIED justified} {@link #setMainAlignment(Alignment) main alignment})
+	 * @return This layout
+	 */
+	public JustifiedBoxLayout stretchEqual(boolean stretchEqual) {
+		isStretchingEqual = stretchEqual;
 		return this;
 	}
 
@@ -197,54 +218,69 @@ public class JustifiedBoxLayout implements LayoutManager2 {
 
 	@Override
 	public void layoutContainer(Container parent) {
+		Dimension parentSize = parent.getSize();
+		if (parentSize.width == 0 || parentSize.height == 0)
+			return;
+
 		Insets insets = parent.getInsets();
-		int compCount = 0;
 		int totalLength = isVertical ? insets.top + insets.bottom : insets.left + insets.right;
 		boolean first = true;
+		List<Component> components = new ArrayList<>(parent.getComponentCount());
 		for (Component comp : parent.getComponents()) {
-			if (!comp.isVisible())
-				continue;
+			if (comp.isVisible())
+				components.add(comp);
+		}
+		int[] preferredMainSizes = new int[components.size()];
+		int[] preferredCrossSizes = new int[components.size()];
+		for (int i = 0; i < components.size(); i++) {
 			if (first)
 				first = false;
 			else
 				totalLength += thePadding;
-			compCount++;
-			totalLength += getMain(comp.getPreferredSize());
+			Dimension ps = components.get(i).getPreferredSize();
+			preferredMainSizes[i] = getMain(ps);
+			preferredCrossSizes[i] = getCross(ps);
+			totalLength += preferredMainSizes[i];
 		}
 
+		int parentLength = getMain(parentSize);
+		if (totalLength > parentLength)
+			layoutStretched(parent, components, preferredMainSizes, preferredCrossSizes, totalLength, parentLength);
+		else if (theMainAlign == Alignment.JUSTIFIED && totalLength != parentLength) {
+			if (isStretchingEqual)
+				layoutStretchedEqual(parent, components, preferredMainSizes, preferredCrossSizes, totalLength, parentLength);
+			else
+				layoutStretched(parent, components, preferredMainSizes, preferredCrossSizes, totalLength, parentLength);
+		} else
+			layoutDistributed(parent, components, preferredMainSizes, preferredCrossSizes, totalLength, parentLength);
+	}
+
+	private void layoutDistributed(Container parent, List<Component> components, int[] preferredMainSizes, int[] preferredCrossSizes,
+		int preferredLength, int parentLength) {
+		Insets insets = parent.getInsets();
 		int margin = isVertical ? insets.top : insets.left;
 		int pos = margin;
-		int pad = 0;
-		float stretch = 1;
+		int pad = thePadding;
 		switch (theMainAlign) {
 		case LEADING:
-			pos = 0;
 			break;
 		case CENTER:
-			pad = (getMain(parent.getSize()) - totalLength) / (compCount + 1);
-			pos = pad;
+			pad = (parentLength - preferredLength) / (components.size() + 1);
 			break;
 		case TRAILING:
-			pos = getMain(parent.getSize()) - totalLength;
+			pos = parentLength - preferredLength;
 			break;
 		case JUSTIFIED:
-			pos = 0;
-			stretch = getMain(parent.getSize()) * 1.0f / totalLength;
 			break;
 		}
-		if (pos < margin)
-			pos = margin;
 		Rectangle bounds = new Rectangle();
-		for (Component comp : parent.getComponents()) {
-			if (!comp.isVisible())
-				continue;
-			Dimension ps = comp.getPreferredSize();
-			int main = getMain(ps);
-			int cross = getCross(ps);
-			int parentCross = getCross(parent.getSize()) - (isVertical ? insets.left + insets.right : insets.top + insets.bottom);
-			setBound(true, bounds, pos, Math.round(main * stretch));
+		int parentCross = getCross(parent.getSize()) - (isVertical ? insets.left + insets.right : insets.top + insets.bottom);
+		int crossMargin = isVertical ? insets.left : insets.top;
+		for (int i = 0; i < components.size(); i++) {
+			int main = preferredMainSizes[i];
+			int cross = Math.max(preferredCrossSizes[i], parentCross);
+			setBound(true, bounds, pos, main);
 			pos += getMain(bounds.getSize()) + pad;
-			int crossMargin = isVertical ? insets.left : insets.top;
 			switch (theCrossAlign) {
 			case LEADING:
 				setBound(false, bounds, crossMargin, cross);
@@ -259,8 +295,126 @@ public class JustifiedBoxLayout implements LayoutManager2 {
 				setBound(false, bounds, crossMargin, parentCross);
 				break;
 			}
-			comp.setBounds(bounds);
-			pos += thePadding;
+			components.get(i).setBounds(bounds);
+		}
+	}
+
+	private void layoutStretched(Container parent, List<Component> components, int[] preferredMainSizes, int[] preferredCrossSizes,
+		int preferredLength, int parentLength) {
+		boolean shrink = parentLength < preferredLength;
+		float[] sizes = new float[components.size()];
+		long totalExtremeSize = 0;
+		for (int i = 0; i < components.size(); i++) {
+			int size = getMain(shrink ? components.get(i).getMinimumSize() : components.get(i).getMaximumSize());
+			sizes[i] = size;
+			totalExtremeSize += size;
+		}
+
+		Insets insets = parent.getInsets();
+		int margin = isVertical ? insets.top : insets.left;
+		float padding = thePadding;
+		int extraLength = margin * 2 + thePadding * (sizes.length - 1);
+		if (shrink == (totalExtremeSize + extraLength < parentLength)) {
+			// The extreme size is more than sufficient to fit in or expand to the parent length
+			// Temper the extreme size to the right amount
+			float num = parentLength - preferredLength;
+			float den = totalExtremeSize - (preferredLength - extraLength);
+			for (int i = 0; i < sizes.length; i++)
+				sizes[i] = preferredMainSizes[i] + num * (sizes[i] - preferredMainSizes[i]) / den;
+		} else {
+			// The extreme size is not big/small enough to fit in or expand to the parent length
+			// Modify the margin and padding accordingly
+			int diff = parentLength - (int) totalExtremeSize;
+			if (!shrink) {
+				float padMod = diff * 1.0f / (sizes.length + 1);
+				margin += (int) padMod;
+				padding += padMod;
+			} else {
+				float padMod = diff * 1.0f / (sizes.length + 1);
+				if (margin > 0) {
+					int padModInt = (int) padMod;
+					margin += padModInt;
+					diff -= padModInt * 2;
+					if (margin < 0) {
+						diff -= margin * 2;
+						margin = 0;
+					}
+					padMod = diff * 1.0f / (sizes.length - 1);
+				}
+				if (padding > 0 && sizes.length > 1) {
+					padding += padMod;
+					diff = 0;
+					if (padding < 0) {
+						diff = Math.round(-padding * (sizes.length - 1));
+						padding = 0;
+					}
+				}
+				if (diff > 0 && margin > 0)
+					margin = Math.max(margin - (diff + 1) / 2, 0);
+			}
+		}
+		float fPos = margin;
+		Rectangle bounds = new Rectangle();
+		int parentCross = getCross(parent.getSize()) - (isVertical ? insets.left + insets.right : insets.top + insets.bottom);
+		int crossMargin = isVertical ? insets.left : insets.top;
+		int pos = margin;
+		for (int i = 0; i < components.size(); i++) {
+			float newFPos = fPos + sizes[i];
+			int mainSize = Math.round(newFPos) - pos;
+			setBound(true, bounds, pos, mainSize);
+			newFPos += padding;
+			fPos = newFPos;
+			pos = Math.round(newFPos);
+
+			int cross = Math.max(preferredCrossSizes[i], parentCross);
+			switch (theCrossAlign) {
+			case LEADING:
+				setBound(false, bounds, crossMargin, cross);
+				break;
+			case CENTER:
+				setBound(false, bounds, (parentCross - cross) / 2, cross);
+				break;
+			case TRAILING:
+				setBound(false, bounds, parentCross - cross - crossMargin, cross);
+				break;
+			case JUSTIFIED:
+				setBound(false, bounds, crossMargin, parentCross);
+				break;
+			}
+			components.get(i).setBounds(bounds);
+		}
+	}
+
+	private void layoutStretchedEqual(Container parent, List<Component> components, int[] preferredMainSizes, int[] preferredCrossSizes,
+		int preferredLength, int parentLength) {
+		Insets insets = parent.getInsets();
+		int margin = isVertical ? insets.top : insets.left;
+		int extraLenth = isVertical ? insets.top + insets.bottom : insets.left + insets.right + thePadding * (components.size() - 1);
+		int pos = margin;
+		float stretch = (parentLength - extraLenth) * 1.0f / (preferredLength - extraLenth);
+		Rectangle bounds = new Rectangle();
+		int parentCross = getCross(parent.getSize()) - (isVertical ? insets.left + insets.right : insets.top + insets.bottom);
+		int crossMargin = isVertical ? insets.left : insets.top;
+		for (int i = 0; i < components.size(); i++) {
+			int main = Math.round(preferredMainSizes[i] * stretch);
+			int cross = Math.max(preferredCrossSizes[i], parentCross);
+			setBound(true, bounds, pos, main);
+			pos += getMain(bounds.getSize()) + thePadding;
+			switch (theCrossAlign) {
+			case LEADING:
+				setBound(false, bounds, crossMargin, cross);
+				break;
+			case CENTER:
+				setBound(false, bounds, (parentCross - cross) / 2, cross);
+				break;
+			case TRAILING:
+				setBound(false, bounds, parentCross - cross - crossMargin, cross);
+				break;
+			case JUSTIFIED:
+				setBound(false, bounds, crossMargin, parentCross);
+				break;
+			}
+			components.get(i).setBounds(bounds);
 		}
 	}
 
