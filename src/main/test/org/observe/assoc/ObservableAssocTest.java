913e1e3b25a590c7d94c0ecc7e562ca47d247bab
abutler <abutler@psl.nmsu.edu>
2019-11-11 11:47:19 -0700

    Removed ObservableTree.  The API was not well thought out, and at the moment, I'm thinking that the prevalence of use cases for uniform, variable level trees will not justify the complexity and effort to create, test, and maintain this type.  Even if I decide to do it later, I think it will probably be better to rewrite it anyway.

--- src/main/test/org/observe/assoc/ObservableAssocTest.java
+++ src/main/test/org/observe/assoc/ObservableAssocTest.java
@@ -1,6 +1,5 @@
 package org.observe.assoc;
 
-import static java.util.Arrays.asList;
 import static org.observe.collect.ObservableCollectionsTest.intType;
 
 import java.util.ArrayList;
@@ -11,15 +10,12 @@ import java.util.function.BiFunction;
 import java.util.function.Function;
 
 import org.junit.Test;
-import org.observe.ObservableValue;
-import org.observe.SimpleSettableValue;
 import org.observe.assoc.ObservableGraph.Edge;
 import org.observe.assoc.ObservableGraph.Node;
 import org.observe.assoc.impl.DefaultObservableGraph;
 import org.observe.collect.ObservableCollection;
 import org.observe.collect.ObservableCollectionTester;
 
-import com.google.common.reflect.TypeParameter;
 import com.google.common.reflect.TypeToken;
 
 /** Runs tests on the data structures built on top of observable collections. */
@@ -183,78 +179,5 @@ public class ObservableAssocTest {
 		nodeChecker.check();
 		edgeChecker.check();
 		edgeCheckers.forEach(ec -> ec.check());
-	}
-
-	class TreeNode<T> {
-		final SimpleSettableValue<T> value;
-		final ObservableCollection<TreeNode<T>> children;
-
-		TreeNode(TypeToken<T> type, T initValue) {
-			value = new SimpleSettableValue<>(type, false);
-			children = ObservableCollection.create(new TypeToken<TreeNode<T>>() {}.where(new TypeParameter<T>() {}, type));
-
-			value.set(initValue, null);
-		}
-
-		TreeNode<T> addChild(T... childValues) {
-			for (T childValue : childValues)
-				children.add(new TreeNode<>(value.getType(), childValue));
-			return this;
-		}
-	}
-
-	/** Tests {@link ObservableTree#valuePathsOf(ObservableTree, boolean)} */
-	// @Test
-	public void testTreeValuePaths() {
-		TypeToken<Integer> type=TypeToken.of(Integer.class);
-		TreeNode<Integer> root=new TreeNode<>(type, 0);
-		root.addChild(1, 2, 3);
-		root.children.get(0).addChild(4, 7, 10);
-		root.children.get(1).addChild(5, 8, 11);
-		root.children.get(2).addChild(6, 9, 12);
-
-		ObservableTree<TreeNode<Integer>, Integer> tree = ObservableTree.of(
-			ObservableValue.of(root.children.getType(), root), type,
-			n -> n.value, n -> n.children);
-
-		ObservableCollection<List<Integer>> allPaths = ObservableTree.valuePathsOf(tree, false);
-		ObservableCollection<List<Integer>> terminalPaths = ObservableTree.valuePathsOf(tree, true);
-		ObservableCollectionTester<List<Integer>> allPathsTester = new ObservableCollectionTester<>("allPaths", allPaths);
-		ObservableCollectionTester<List<Integer>> terminalPathsTester = new ObservableCollectionTester<>("terminalPaths", terminalPaths);
-
-		allPathsTester.set(//
-			asList(0), //
-			asList(0, 1), asList(0, 2), asList(0, 3), //
-			asList(0, 1, 4), asList(0, 1, 7), asList(0, 1, 10), //
-			asList(0, 2, 5), asList(0, 2, 8), asList(0, 2, 11), //
-			asList(0, 3, 6), asList(0, 3, 9), asList(0, 3, 12));
-		terminalPathsTester.set(//
-			asList(0, 1, 4), asList(0, 1, 7), asList(0, 1, 10), //
-			asList(0, 2, 5), asList(0, 2, 8), asList(0, 2, 11), //
-			asList(0, 3, 6), asList(0, 3, 9), asList(0, 3, 12));
-
-		allPathsTester.check();
-		terminalPathsTester.check();
-
-		root.children.get(1).children.get(1).addChild(12, 15, 18);
-		allPathsTester.add(//
-			asList(0, 2, 8, 12), asList(0, 2, 8, 15), asList(0, 2, 8, 18));
-		terminalPathsTester.remove(asList(0, 2, 8)).add(//
-			asList(0, 2, 8, 12), asList(0, 2, 8, 15), asList(0, 2, 8, 18));
-
-		allPathsTester.check();
-		terminalPathsTester.check();
-
-		root.children.remove(1);
-		allPathsTester.removeAll(//
-			asList(0, 2), //
-			asList(0, 2, 5), asList(0, 2, 8), asList(0, 2, 11), //
-			asList(0, 2, 8, 12), asList(0, 2, 8, 15), asList(0, 2, 8, 18));
-		terminalPathsTester.removeAll(//
-			asList(0, 2, 5), asList(0, 2, 11), //
-			asList(0, 2, 8, 12), asList(0, 2, 8, 15), asList(0, 2, 8, 18));
-
-		allPathsTester.check();
-		terminalPathsTester.check();
 	}
 }
