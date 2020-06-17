9581e70a3b9990f19bd2f7b0d94da53d7bd53458
abutler <abutler@psl.nmsu.edu>
2020-06-17 13:07:13 -0600

    Added a few utilities to TypeTokens

--- src/main/java/org/observe/util/TypeTokens.java
+++ src/main/java/org/observe/util/TypeTokens.java
@@ -1,6 +1,9 @@
 package org.observe.util;
 
+import java.lang.annotation.Annotation;
+import java.lang.reflect.AnnotatedType;
 import java.lang.reflect.GenericArrayType;
+import java.lang.reflect.GenericDeclaration;
 import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Type;
 import java.lang.reflect.TypeVariable;
@@ -10,6 +13,7 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.NavigableMap;
@@ -20,6 +24,7 @@ import java.util.SortedSet;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.function.BiFunction;
 import java.util.function.Function;
+import java.util.function.Supplier;
 
 import org.qommons.BiTuple;
 import org.qommons.TriTuple;
@@ -43,7 +48,6 @@ import com.google.common.reflect.TypeToken;
  * getRawType()}. This class provides some optimizations as well as boilerplate-reducing utilities.
  */
 public class TypeTokens {
-	private static final TypeTokens instance = new TypeTokens();
 	static final Map<Class<?>, UnaryCompoundTypeCreator<?>> STANDARD_UNARY_TYPE_CREATORS;
 	static final Map<Class<?>, BinaryCompoundTypeCreator<?>> STANDARD_BINARY_TYPE_CREATORS;
 	static final Map<Class<?>, TernaryCompoundTypeCreator<?>> STANDARD_TERNARY_TYPE_CREATORS;
@@ -59,6 +63,8 @@ public class TypeTokens {
 		STANDARD_BINARY_TYPE_CREATORS = Collections.unmodifiableMap(binaryTypes);
 		STANDARD_TERNARY_TYPE_CREATORS = Collections.unmodifiableMap(ternaryTypes);
 	}
+
+	private static final TypeTokens instance = new TypeTokens();
 
 	@SuppressWarnings("rawtypes")
 	private static void populateStandardCompoundTypes(Map<Class<?>, UnaryCompoundTypeCreator<?>> unaryTypes,
@@ -157,29 +163,28 @@ public class TypeTokens {
 		binaryTypes.put(Map.class, new BinaryCompoundTypeCreator<Map>() {
 			@Override
 			public <P1, P2> TypeToken<? extends Map> createCompoundType(TypeToken<P1> param1, TypeToken<P2> param2) {
-				return new TypeToken<Map<P1, P2>>() {}.where(new TypeParameter<P1>() {}, param1)
-					.where(new TypeParameter<P2>() {}, param2);
+				return new TypeToken<Map<P1, P2>>() {}.where(new TypeParameter<P1>() {}, param1).where(new TypeParameter<P2>() {}, param2);
 			}
 		});
 		binaryTypes.put(SortedMap.class, new BinaryCompoundTypeCreator<SortedMap>() {
 			@Override
 			public <P1, P2> TypeToken<? extends SortedMap> createCompoundType(TypeToken<P1> param1, TypeToken<P2> param2) {
-				return new TypeToken<SortedMap<P1, P2>>() {}.where(new TypeParameter<P1>() {}, param1)
-					.where(new TypeParameter<P2>() {}, param2);
+				return new TypeToken<SortedMap<P1, P2>>() {}.where(new TypeParameter<P1>() {}, param1).where(new TypeParameter<P2>() {},
+					param2);
 			}
 		});
 		binaryTypes.put(NavigableMap.class, new BinaryCompoundTypeCreator<NavigableMap>() {
 			@Override
 			public <P1, P2> TypeToken<? extends NavigableMap> createCompoundType(TypeToken<P1> param1, TypeToken<P2> param2) {
-				return new TypeToken<NavigableMap<P1, P2>>() {}.where(new TypeParameter<P1>() {}, param1)
-					.where(new TypeParameter<P2>() {}, param2);
+				return new TypeToken<NavigableMap<P1, P2>>() {}.where(new TypeParameter<P1>() {}, param1).where(new TypeParameter<P2>() {},
+					param2);
 			}
 		});
 		binaryTypes.put(BetterMap.class, new BinaryCompoundTypeCreator<BetterMap>() {
 			@Override
 			public <P1, P2> TypeToken<? extends BetterMap> createCompoundType(TypeToken<P1> param1, TypeToken<P2> param2) {
-				return new TypeToken<BetterMap<P1, P2>>() {}.where(new TypeParameter<P1>() {}, param1)
-					.where(new TypeParameter<P2>() {}, param2);
+				return new TypeToken<BetterMap<P1, P2>>() {}.where(new TypeParameter<P1>() {}, param1).where(new TypeParameter<P2>() {},
+					param2);
 			}
 		});
 		binaryTypes.put(BetterSortedMap.class, new BinaryCompoundTypeCreator<BetterSortedMap>() {
@@ -192,8 +197,8 @@ public class TypeTokens {
 		binaryTypes.put(MultiMap.class, new BinaryCompoundTypeCreator<MultiMap>() {
 			@Override
 			public <P1, P2> TypeToken<? extends MultiMap> createCompoundType(TypeToken<P1> param1, TypeToken<P2> param2) {
-				return new TypeToken<MultiMap<P1, P2>>() {}.where(new TypeParameter<P1>() {}, param1)
-					.where(new TypeParameter<P2>() {}, param2);
+				return new TypeToken<MultiMap<P1, P2>>() {}.where(new TypeParameter<P1>() {}, param1).where(new TypeParameter<P2>() {},
+					param2);
 			}
 		});
 		binaryTypes.put(BetterMultiMap.class, new BinaryCompoundTypeCreator<BetterMultiMap>() {
@@ -213,15 +218,15 @@ public class TypeTokens {
 		binaryTypes.put(BiTuple.class, new BinaryCompoundTypeCreator<BiTuple>() {
 			@Override
 			public <P1, P2> TypeToken<? extends BiTuple> createCompoundType(TypeToken<P1> param1, TypeToken<P2> param2) {
-				return new TypeToken<BiTuple<P1, P2>>() {}.where(new TypeParameter<P1>() {}, param1)
-					.where(new TypeParameter<P2>() {}, param2);
+				return new TypeToken<BiTuple<P1, P2>>() {}.where(new TypeParameter<P1>() {}, param1).where(new TypeParameter<P2>() {},
+					param2);
 			}
 		});
 		binaryTypes.put(Function.class, new BinaryCompoundTypeCreator<Function>() {
 			@Override
 			public <P1, P2> TypeToken<? extends Function> createCompoundType(TypeToken<P1> param1, TypeToken<P2> param2) {
-				return new TypeToken<Function<P1, P2>>() {}.where(new TypeParameter<P1>() {}, param1)
-					.where(new TypeParameter<P2>() {}, param2);
+				return new TypeToken<Function<P1, P2>>() {}.where(new TypeParameter<P1>() {}, param1).where(new TypeParameter<P2>() {},
+					param2);
 			}
 		});
 
@@ -229,8 +234,8 @@ public class TypeTokens {
 			@Override
 			public <P1, P2, P3> TypeToken<? extends TriTuple> createCompoundType(TypeToken<P1> param1, TypeToken<P2> param2,
 				TypeToken<P3> param3) {
-				return new TypeToken<TriTuple<P1, P2, P3>>() {}.where(new TypeParameter<P1>() {}, param1)
-					.where(new TypeParameter<P2>() {}, param2);
+				return new TypeToken<TriTuple<P1, P2, P3>>() {}.where(new TypeParameter<P1>() {}, param1).where(new TypeParameter<P2>() {},
+					param2);
 			}
 		});
 		ternaryTypes.put(BiFunction.class, new TernaryCompoundTypeCreator<BiFunction>() {
@@ -272,6 +277,7 @@ public class TypeTokens {
 		public final List<T> enumConstants;
 		private Object theCompoundTypeCreator;
 		private Map<List<TypeToken<?>>, TypeToken<? extends T>> theCompoundTypes;
+		private Map<Object, Object> theCache;
 
 		TypeKey(Class<T> clazz) {
 			this.clazz = clazz;
@@ -419,6 +425,12 @@ public class TypeTokens {
 			return (TypeToken<P>) parameterizedType;
 		}
 
+		public synchronized <X> X getCached(Object key, Supplier<X> supplier) {
+			if (theCache == null)
+				theCache = new HashMap<>();
+			return (X) theCache.computeIfAbsent(key, __ -> supplier.get());
+		}
+
 		@Override
 		public int hashCode() {
 			return clazz.hashCode();
@@ -459,6 +471,7 @@ public class TypeTokens {
 	public final TypeToken<Object> OBJECT;
 	public final TypeToken<Void> VOID;
 	public final TypeToken<?> WILDCARD;
+	public final TypeToken<Class<?>> CLASS;
 
 	/** Creates a new instance */
 	protected TypeTokens() {
@@ -499,6 +512,7 @@ public class TypeTokens {
 		OBJECT = of(Object.class);
 		VOID = of(Void.class);
 		WILDCARD = TypeToken.of(new WildcardTypeImpl());
+		CLASS = keyFor(Class.class).parameterized();
 		// WILDCARD = new TypeToken<Class<?>>() {}.resolveType(Class.class.getTypeParameters()[0]);
 	}
 
@@ -613,10 +627,205 @@ public class TypeTokens {
 		return rawType.isInstance(value);
 	}
 
-	public <T> T cast(TypeToken<T> type, Object value) {
+	public <T> T cast(TypeToken<T> type, Object value) throws ClassCastException {
 		if (value != null && !(isInstance(type, value)))
 			throw new ClassCastException("Cannot cast instance of " + value.getClass().getName() + " to " + type);
 		return (T) value;
+	}
+
+	/**
+	 * Similar to {@link TypeToken#isAssignableFrom(TypeToken)}, but this method allows for assignments where conversion is required, e.g.
+	 * auto-(un)boxing and primitive number type conversion.
+	 *
+	 * @param left The type of the variable to assign the value to
+	 * @param right The type of the value to assign
+	 * @return Whether the given value type can be assigned to the given variable type
+	 */
+	public boolean isAssignable(TypeToken<?> left, TypeToken<?> right) {
+		if (left.isAssignableFrom(right))
+			return true;
+		Class<?> primitiveRight = unwrap(getRawType(right));
+		Class<?> primitiveLeft = unwrap(getRawType(left));
+		if (!primitiveRight.isPrimitive())
+			return false;
+		else if (primitiveRight.equals(primitiveLeft))
+			return true;
+		else if (primitiveRight == boolean.class)
+			return false;
+		else if (primitiveLeft == Number.class)
+			return true;
+		else if (!primitiveLeft.isPrimitive())
+			return false;
+		else if (primitiveLeft == double.class)
+			return true;
+		// Now left!=right and left!=double.class
+		else if (primitiveRight == double.class || primitiveRight == float.class || primitiveRight == long.class)
+			return false;
+		// Now right can only be int, short, byte, or char
+		else if (primitiveLeft == long.class || primitiveLeft == int.class)
+			return true;
+		else if (primitiveRight == byte.class)
+			return primitiveLeft == short.class || primitiveLeft == char.class;
+		else
+			return false;
+	}
+
+	public <T, X> Function<T, X> getCast(TypeToken<T> left, TypeToken<X> right) {
+		if (left.isAssignableFrom(right)) {
+			if (left.isPrimitive() && !right.isPrimitive())
+				return v -> {
+					if (v == null)
+						throw new NullPointerException("Cannot cast null to primitive type " + left);
+					return (X) v;
+				};
+				else
+					return v -> (X) v;
+		}
+		Class<?> primitiveRight = unwrap(getRawType(right));
+		Class<?> primitiveLeft = unwrap(getRawType(left));
+		Function<T, T> nullCheck;
+		if (left.isPrimitive() && !right.isPrimitive()) {
+			nullCheck = v -> {
+				if (v == null)
+					throw new NullPointerException("Cannot cast null to primitive type " + left);
+				return v;
+			};
+		} else {
+			nullCheck = v -> v;
+		}
+		if (!primitiveRight.isPrimitive())
+			throw new IllegalArgumentException("Cannot cast " + right + " to " + left);
+		else if (primitiveRight == primitiveLeft) {
+			return v -> (X) nullCheck.apply(v);
+		} else if (!primitiveLeft.isPrimitive())
+			throw new IllegalArgumentException("Cannot cast " + right + " to " + left);
+		else if (primitiveRight == boolean.class)
+			throw new IllegalArgumentException("Cannot cast " + right + " to " + left);
+		Function<T, Number> toNumber;
+		if (primitiveRight == char.class) {
+			toNumber = v -> Integer.valueOf(((Character) nullCheck.apply(v)).charValue());
+		} else {
+			toNumber = v -> (Number) nullCheck.apply(v);
+		}
+		Function<T, Object> fn;
+		if (primitiveLeft == double.class) {
+			fn = v -> toNumber.apply(v).doubleValue();
+			// Now left!=right and left!=double.class
+		} else if (primitiveRight == double.class || primitiveRight == float.class || primitiveRight == long.class)
+			throw new IllegalArgumentException("Cannot cast " + right + " to " + left);
+		// Now right can only be int, short, byte, or char
+		else if (primitiveLeft == long.class) {
+			fn = v -> toNumber.apply(v).longValue();
+		} else if (primitiveLeft == int.class) {
+			fn = v -> toNumber.apply(v).intValue();
+		} else if (primitiveRight == byte.class) {
+			if (primitiveLeft == short.class) {
+				fn = v -> toNumber.apply(v).shortValue();
+			} else if (primitiveLeft == char.class) {
+				fn = v -> Character.valueOf((char) toNumber.apply(v).byteValue());
+			} else
+				throw new IllegalArgumentException("Cannot cast " + right + " to " + left);
+		} else
+			throw new IllegalArgumentException("Cannot cast " + right + " to " + left);
+
+		return (Function<T, X>) fn;
+	}
+
+	public <X> X castAssignable(TypeToken<X> type, Object value) throws ClassCastException {
+		if (value == null) {
+			if (type.isPrimitive())
+				throw new ClassCastException("Null cannot be cast to primitive type " + type);
+			else
+				return null;
+		} else if (!isAssignable(type, TypeTokens.get().of(value.getClass())))
+			throw new ClassCastException("Cannot cast instance of " + value.getClass().getName() + " to " + type);
+		return (X) value;
+	}
+
+	public <X> TypeToken<X> getCommonType(TypeToken<? extends X>... types) {
+		return getCommonType(Arrays.asList(types));
+	}
+
+	public <X> TypeToken<X> getCommonType(List<TypeToken<? extends X>> types) {
+		return (TypeToken<X>) _commonType(types);
+	}
+
+	private <X> TypeToken<?> _commonType(List<TypeToken<? extends X>> types) {
+		TypeToken<? extends X> firstType = types.get(0);
+		boolean first = true;
+		boolean firstIsCommon = true;
+		for (TypeToken<? extends X> type : types) {
+			if (first)
+				first = false;
+			else if (!isAssignable(firstType, type)) {
+				firstIsCommon = false;
+				break;
+			}
+		}
+		if (firstIsCommon)
+			return firstType;
+		List<? extends TypeToken<?>> extendedTypes = decompose(firstType);
+		for (TypeToken<?> extType : extendedTypes) {
+			first = true;
+			boolean extIsCommon = true;
+			for (TypeToken<? extends X> type : types) {
+				if (first)
+					first = false;
+				else if (!isAssignable(extType, type)) {
+					extIsCommon = false;
+					break;
+				}
+			}
+			if (extIsCommon)
+				return extType;
+		}
+		return OBJECT;
+	}
+
+	public <X> List<TypeToken<? super X>> decompose(TypeToken<X> type) {
+		List<TypeToken<? super X>> decomposed = new LinkedList<>();
+		Class<?> raw = getRawType(type);
+		if (raw.isPrimitive()) {
+			decomposed.add(wrap(type));
+			if (raw != boolean.class)
+				decomposed.add((TypeToken<? super X>) of(Number.class));
+			while (true) {
+				decomposed.add((TypeToken<? super X>) of(raw));
+				if (raw == byte.class) {
+					decomposed.add((TypeToken<? super X>) of(char.class));
+					raw = short.class;
+				} else if (raw == short.class)
+					raw = int.class;
+				else if (raw == int.class) {
+					decomposed.add((TypeToken<? super X>) of(float.class));
+					decomposed.add((TypeToken<? super X>) of(double.class));
+					raw = long.class;
+				} else if (raw == float.class)
+					raw = double.class;
+				else
+					break;
+			}
+		}
+		if (!raw.isInterface()) {
+			Class<X> c = (Class<X>) raw;
+			TypeToken<X> t = type;
+			while (raw != Object.class) {
+				decomposed.add(t);
+				c = (Class<X>) c.getSuperclass();
+				t = (TypeToken<X>) t.getSupertype(c);
+			}
+		}
+		for (Class<?> intf : raw.getInterfaces())
+			this.<Object, X> decomposeInterfaces(decomposed, (Class<Object>) intf,
+				(TypeToken<Object>) type.getSupertype((Class<Object>) intf));
+		return decomposed;
+	}
+
+	private <I, X extends I> void decomposeInterfaces(List<TypeToken<? super X>> decomposed, Class<I> intf, TypeToken<I> intfType) {
+		decomposed.add(intfType);
+		for (Class<?> intf2 : intf.getInterfaces())
+			this.<Object, X> decomposeInterfaces(decomposed, (Class<Object>) intf2,
+				(TypeToken<Object>) intfType.getSupertype((Class<Object>) intf2));
 	}
 
 	public boolean isBoolean(TypeToken<?> type) {
@@ -674,15 +883,139 @@ public class TypeTokens {
 		return keyFor(type).comparable;
 	}
 
+	public TypeToken<?> getArrayType(TypeToken<?> componentType, int dimCount) {
+		return _arrayType(componentType, dimCount);
+	}
+
+	private <X> TypeToken<?> _arrayType(TypeToken<X> componentType, int dimCount) {
+		if (dimCount == 0)
+			return componentType;
+		else if (dimCount < 0)
+			throw new IllegalArgumentException("Negative dimension count: " + dimCount);
+		TypeToken<X> type = componentType;
+		if (type.isPrimitive()) {
+			Class<X> raw = getRawType(type);
+			if (raw == boolean.class)
+				type = (TypeToken<X>) of(boolean[].class);
+			else if (raw == char.class)
+				type = (TypeToken<X>) of(char[].class);
+			else if (raw == byte.class)
+				type = (TypeToken<X>) of(byte[].class);
+			else if (raw == short.class)
+				type = (TypeToken<X>) of(byte[].class);
+			else if (raw == int.class)
+				type = (TypeToken<X>) of(byte[].class);
+			else if (raw == long.class)
+				type = (TypeToken<X>) of(byte[].class);
+			else if (raw == float.class)
+				type = (TypeToken<X>) of(byte[].class);
+			else if (raw == double.class)
+				type = (TypeToken<X>) of(byte[].class);
+			else
+				throw new IllegalStateException("Unrecognized primitive type: " + type);
+
+			dimCount--;
+		}
+		for (int i = 0; i < dimCount; i++) {
+			type = (TypeToken<X>) new TypeToken<X[]>() {}.where(new TypeParameter<X>() {}, type);
+		}
+		return type;
+	}
+
+	public <D extends GenericDeclaration, X> TypeToken<X> getTypeVariable(String variableName, D declaration) {
+		return (TypeToken<X>) TypeToken.of(new TypeVariableImpl<>(declaration, variableName));
+	}
+
+	public <X> TypeToken<? extends X> getExtendsWildcard(TypeToken<X> type) {
+		return (TypeToken<? extends X>) TypeToken.of(new WildcardTypeImpl(new Type[0], new Type[] { type.getType() }));
+	}
+
+	public <X> TypeToken<? super X> getSuperWildcard(TypeToken<X> type) {
+		return (TypeToken<? super X>) TypeToken.of(new WildcardTypeImpl(new Type[] { type.getType() }, new Type[0]));
+	}
+
 	private static class WildcardTypeImpl implements WildcardType {
+		private final Type[] theLowerBounds;
+		private final Type[] theUpperBounds;
+
+		public WildcardTypeImpl() {
+			this(new Type[0], new Type[0]);
+		}
+
+		public WildcardTypeImpl(Type[] lowerBounds, Type[] upperBounds) {
+			theLowerBounds = lowerBounds;
+			theUpperBounds = upperBounds;
+		}
+
 		@Override
 		public Type[] getUpperBounds() {
-			return new Type[0];
+			return theUpperBounds;
 		}
 
 		@Override
 		public Type[] getLowerBounds() {
+			return theLowerBounds;
+		}
+	}
+
+	private static class TypeVariableImpl<D extends GenericDeclaration> implements TypeVariable<D> {
+		private final D theDeclaration;
+		private final String theName;
+
+		TypeVariableImpl(D declaration, String name) {
+			theDeclaration = declaration;
+			theName = name;
+		}
+
+		@Override
+		public <T extends Annotation> T getAnnotation(Class<T> annotationClass) {
+			return null;
+		}
+
+		@Override
+		public Annotation[] getAnnotations() {
+			return new Annotation[0];
+		}
+
+		@Override
+		public Annotation[] getDeclaredAnnotations() {
+			return new Annotation[0];
+		}
+
+		@Override
+		public Type[] getBounds() {
 			return new Type[0];
+		}
+
+		@Override
+		public D getGenericDeclaration() {
+			return theDeclaration;
+		}
+
+		@Override
+		public String getName() {
+			return theName;
+		}
+
+		@Override
+		public AnnotatedType[] getAnnotatedBounds() {
+			return new AnnotatedType[0];
+		}
+
+		@Override
+		public int hashCode() {
+			return theName.hashCode();
+		}
+
+		@Override
+		public boolean equals(Object obj) {
+			return obj instanceof TypeVariableImpl && theDeclaration.equals(((TypeVariableImpl<?>) obj).theDeclaration)
+				&& theName.equals(((TypeVariableImpl<?>) obj).theName);
+		}
+
+		@Override
+		public String toString() {
+			return theName;
 		}
 	}
 }
