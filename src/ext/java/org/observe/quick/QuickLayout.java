1feb41f69f7f979cb011cc7c69161eedffff13f5
Andrew Butler <abutler@sgc.nmsu.edu>
2023-10-26 07:55:36 -0600

    Deleting the old Quick code.  What I've got now is at this point, I think, better in every way

--- src/ext/java/org/observe/quick/QuickLayout.java
+++
@@ -1,9 +0,0 @@
-package org.observe.quick;
-
-import java.awt.LayoutManager;
-
-/** A layout defined in Quick */
-public interface QuickLayout {
-	/** @return The swing layout for the container (box) */
-	LayoutManager create();
-}
